68d5b705c6ab2ba0c34373832a654fa60cbf8be6
vitor <josevitoroff@gmail.com>
2024-04-08 19:49:00 -0300

    criado funções de mudar canal e get info e adicionado caracteristicas da televisão

--- Tv/tv.py
+++ Tv/tv.py
@@ -1,6 +1,11 @@
 class TV:
 
-    def __init__(self):
+    def __init__(self, marca, cor, ano_fabricacao, polegadas, qualidade_imagem):
+        self.marca = marca
+        self.cor = cor
+        self.ano_fabricacao = ano_fabricacao
+        self.polegadas = polegadas
+        self.qualidade_imagem = qualidade_imagem
         self.power = False
         self.volume = 50
         self.canal = 1
@@ -14,8 +19,8 @@ class TV:
     def Aumentarvolume(self):
         if self.mudo == True:
             self.volume = self.volume_anterior
+            self.mudo = False
             print('removido do mudo')
-
         if self.volume == 100:
             print('o volume está no maximo')
         else:
@@ -25,8 +30,8 @@ class TV:
     def Diminuirvolume(self):
         if self.mudo == True:
             self.volume = self.volume_anterior
+            self.mudo = False
             print('removido do mudo')
-
         if self.volume == 0:
             print('o volume está no minimo')
         else:
@@ -52,10 +57,28 @@ class TV:
         return self.mudo
         # return print('mutado')
 
+    def mudar_canal(self, num):
+        self.canal = num
+        return self.canal
 
-controle1 = TV()
+    def Get_info(self):
+        print("Informações da TV")
+        print("Marca:", self.marca)
+        print("Cor:", self.cor)
+        print("Ano de Fabricacao:", self.ano_fabricacao)
+        print("Polegadas:", self.polegadas)
+        print("Qualidade de Imagem:", self.qualidade_imagem)
+
+
+controle1 = TV(marca="LG", cor="preta", ano_fabricacao=2024,
+               polegadas=55, qualidade_imagem="4K")
+
+print(controle1.Get_info())
 print(controle1.Power())
 print(controle1.Diminuirvolume())
+print(controle1.Proximocanal())
 print(controle1.Diminuirvolume())
 print(controle1.Mudo())
+print(controle1.Aumentarvolume())
+print(controle1.mudar_canal(7))
 print(controle1.Aumentarvolume())
